e907a297394eb8afe06bee0b1c9e1de39b633997
St. John Peaster <stjohn3d@outlook.com>
2015-06-03 14:25:10 -0400

    The build script now runs a test file to validate the installation.
    I should probably rename build to install...

---
+++ ai-deftly/testInstall.jsx
@@ -0,0 +1,18 @@
+#target illustrator
+
+var moduleNotFound = function(name){
+    alert("Failed to load module: " + name);
+    return false;
+}
+
+var test = function() {
+    if (!Console) return moduleNotFound("Console");
+    if (!Ensure) return moduleNotFound("Ensure");
+    if (!Obj) return moduleNotFound("Obj");
+    if (!JSON) return moduleNotFound("JSON");
+    if (!Ai) return moduleNotFound("Ai");
+    return true;
+}
+
+if (test()) alert("Creating Deftly installed successfully");
+app.quit();
