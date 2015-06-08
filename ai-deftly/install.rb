f884e98fe039221006ca26a7141695d37f28a837
St. John <stjohn3d@outlook.com>
2015-06-08 08:58:58 -0400

    Windows Support WIP
    Basic install uninstall support for windows. You MUST launch your
    terminal as an administrator however. Need to add warning to launch
    non-administrative terminals as such. And the testInstall.jsx isn't
    working yet.

--- ai-deftly/install.rb
+++ ai-deftly/install.rb
@@ -8,24 +8,31 @@ if path.include? "\\"
     os = 'WIN'
 end
 
+osPath = '/Applications/Adobe Illustrator CC 2014/Startup Scripts/'
+if os == 'WIN'
+	osPath = "C:/Program Files/Adobe/Adobe Illustrator CC 2014/Startup Scripts/"
+end
+
+if Dir.exists?(osPath)
+    puts "working in #{osPath}"
+    puts "removing any old installations"
+    FileUtils.rm_r(Dir.glob("#{osPath}creatingDeftly*"))
+else
+    Dir.mkdir osPath
+    puts "created Startup Scripts folder in \n#{osPath}"
+end
+
+puts "copying in new installation"
+FileUtils.cp_r(Dir.glob('./creatingDeftly*'), Dir.glob("#{osPath}"))
+
+working_directory = Dir.pwd
+test_file_path = "#{working_directory}/testInstall.jsx"
+puts "running installation test..."
 if os == 'OSX'
-    osPath = '/Applications/Adobe Illustrator CC 2014/Startup Scripts/'
-
-    if Dir.exists?(osPath)
-        puts "working in #{osPath}"
-        puts "removing any old installations"
-        FileUtils.rm_r(Dir.glob("#{osPath}creatingDeftly*"))
-    else
-        Dir.mkdir osPath
-        puts "created Startup Scripts folder in \n#{osPath}"
-    end
-
-    puts "copying in new installation"
-    FileUtils.cp_r(Dir.glob('./creatingDeftly*'), Dir.glob("#{osPath}"))
-
-    working_directory = Dir.pwd
-    test_file_path = "#{working_directory}/testInstall.jsx"
-    puts "running installation test..."
-    `open -W -F -b com.adobe.illustrator #{test_file_path}`
-    puts "Creating Deftly installed successfully!"
+	`open -W -F -b com.adobe.illustrator #{test_file_path}`
+	puts "Creating Deftly installed successfully!"
+end
+if os == 'WIN'
+	`Invoke-Item #{test_file_path.gsub('/', '\\')}`
+	puts "Creating Deftly installed successfully!"
 end
--- ai-deftly/uninstall.rb
+++ ai-deftly/uninstall.rb
@@ -8,14 +8,15 @@ if path.include? "\\"
     os = 'WIN'
 end
 
-if os == 'OSX'
-    osPath = '/Applications/Adobe Illustrator CC 2014/Startup Scripts/'
+osPath = '/Applications/Adobe Illustrator CC 2014/Startup Scripts/'
+if os == 'WIN'
+	osPath = "C:/Program Files/Adobe/Adobe Illustrator CC 2014/Startup Scripts/"
+end
 
-    if Dir.exists?(osPath)
-        puts "uninstalling from #{osPath}"
-        FileUtils.rm_r(Dir.glob("#{osPath}creatingDeftly*"))
-        puts "Creating Deftly has been uninstalled (╯︵╰,)"
-    else
-        puts "nothing to uninstall, done."
-    end
+if Dir.exists?(osPath)
+    puts "uninstalling from #{osPath}"
+    FileUtils.rm_r(Dir.glob("#{osPath}creatingDeftly*"))
+    puts "Creating Deftly has been uninstalled (;.;)"
+else
+    puts "nothing to uninstall, done."
 end
