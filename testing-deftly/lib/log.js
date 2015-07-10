daebb357b18ea6670c81e9b86ec9f4841309d2b1
St. John Peaster <stjohn3d@outlook.com>
2015-07-10 13:02:55 -0400

    logs.js
    Modularized logs.js
    
    Now to write the tests and potentially do some light refactoring

--- testing-deftly/lib/log.js
+++ testing-deftly/lib/log.js
@@ -1,33 +1,62 @@
 'use strict';
-// var testLogsPath = workingDirectory + '/adobe.test';
-// var testLogs = new File(testLogsPath);
-// if (!testLogs.exists) testLogs = new File(testLogsPath);
+/*global File*/
+var log = function(path, name, tabSize, lineFeed) {
+    var logObj = {};
 
-// testLogs.encoding = 'UTF-8';
-// testLogs.lineFeed = 'Unix';
-// // txtFile.lineFeed = "Windows";
-// // txtFile.lineFeed = "Macintosh";
-// var clearTestLogs = function() {
-//     testLogs.open('w');
-//     testLogs.writeln('');
-//     testLogs.close();
-// };
+    logObj.path = path || '~/Dekstop/';
+    logObj.name = name || 'adobe.test';
 
-// var spaces = function(number) {
-//     var spaces = '';
-//     while (number > 0) {
-//         spaces += ' ';
-//         number--;
-//     }
-//     return spaces;
-// };
+    logObj.lineFeed = lineFeed || 'Unix'; //"Windows" "Macintosh"
+    logObj.setLineFeed = function(feedType) { logObj.lineFeed = feedType; return logObj; };
 
-// var writeToTestLogs = function(string) {
-//     testLogs.open('a');
-//     testLogs.writeln(spaces(describes.length * tabSize) + string);
-//     testLogs.close();
-// };
+    var setFile = function(path, name) {
+        var filePath = path + name;
+        var file = new File(filePath);
+        // if (!file.exists) file = new File(filePath);
 
-// var trace = function(string) {
-//     writeToTestLogs('testingDeftly-- ' + string);
-// };
+        file.encoding = 'UTF-8';
+        file.lineFeed = logObj.lineFeed;
+
+        logObj.file = file;
+        return logObj;
+    };
+    logObj.setFile = setFile;
+    setFile(path, name);
+
+    logObj.tabSize = tabSize || 4;
+    logObj.setTabSize = function(size) { logObj.tabSize = size || 4; return logObj; };
+    var tab = function(number) {
+        var spaces = '';
+        while (number > 0) {
+            spaces += ' ';
+            number--;
+        }
+        return spaces;
+    };
+    logObj.tab = tab;
+
+    var writeLn = function(string, tabCount, action) {
+        if (string === undefined || string === null) throw ('log Object: writeLn: ERROR: Must provide a string value');
+        tabCount = tabCount || 0;
+        action = action || 'a'; //'w'
+        logObj.file.open(action);
+        logObj.file.writeln(tab(tabCount * logObj.tabSize) + string);
+        logObj.file.close();
+    };
+    logObj.writeLn = writeLn;
+
+    var trace = function(string) {
+        writeLn('testingDeftly-- ' + string);
+    };
+    logObj.trace = trace;
+
+    var clear = function() {
+        logObj.writeln('', 0, 'w');
+        return logObj;
+    };
+    logObj.clear = clear;
+
+    return logObj;
+};
+
+module.exports = log;
--- testing-deftly/test/log.test.js
+++ testing-deftly/test/log.test.js
@@ -1,2 +1,16 @@
-// var expect = require('chai').expect;
-// var log = require('../lib/log');
+'use strict';
+/*global describe before it*/
+var expect = require('chai').expect;
+var log = require('../lib/log');
+
+describe('log', function() {
+    before(function() {
+        var testLogger = log('./fodder/', 'testLogger.txt');
+    });
+
+    it('should create a loggerObject');
+
+    it('should create testLogger.txt');
+
+    it('should append if the file already exists');
+});
