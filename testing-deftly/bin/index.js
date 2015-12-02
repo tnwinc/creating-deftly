5f1d1d361eae3cb2b450966e46748857fa7b6f1f
St. John Peaster <stjohn3d@outlook.com>
2015-12-02 13:34:20 -0500

    Cleaned up the index.js file
    Removed old comments, improved other comments so they can be easily
    collapsed. Reordered code now that there is proper control of flow
    thanks to execAsyncCMD.js

--- testing-deftly/bin/index.js
+++ testing-deftly/bin/index.js
@@ -3,38 +3,42 @@
 var execAsyncCMD = require('./execAsyncCMD');
 var help = require('./help.js');
 var parseArgs = require('./parseArguments');
-var userArgs = process.argv.slice(2); // eslint-disable-line no-unused-vars
-// SJ: Above we use slice(2) because the first argument is always node,
-//     and the second is the path to the file that has been executed.
+var userArgs = process.argv.slice(2);
+/* SJ: Above we use slice(2) because the first argument is always node,
+   //  and the second is the path to the this node app (/usr/local/bin/tdeftly).
+   //  These arguments will be processed last, after any tdeftly.opts files
+*/
 
 var tdeftly = {
 	help             : false,
 	watch            : false,
 	verbose          : false,
 	testFileExtension: 'spec',
-	projectDirectory : '~/Desktop',  // TODO Get PWD at the beginning of this file
+	projectDirectory : '~/Desktop',
 	time             : require('../lib/Time'),
 	date             : require('../lib/date'),
 	logger           : require('../lib/logger'),
 	brandching       : require('../lib/Branching'),
 	assertions       : require('../lib/Assertions')
 };
-// SJ: Above is the initial config with default options
-//     Some of these can change from user args in the next line,
-//     Others can change from a tdeftly.opts file
-// TODO look for and process opts file before parsing user args below
-parseArgs(userArgs, tdeftly);
+/* SJ: Above is the initial config with default options
+   //  Some of these can change from user args in the next line,
+   //  Others can change from a tdeftly.opts file
+*/
 
 
-if (tdeftly.help || tdeftly.h) help.show(); // eslint-disable-line curly
-else {
-	execAsyncCMD('pwd').then(function(dirPath) {
-		console.log(dirPath);
-	}).onError(function(err) {
-		console.log(err);
-	}).finally(function() {
-		console.log(' - Watching: ' + tdeftly.watch);
-	});
-}
-	// 		// Look for a testingDeftly.opts json file - refer to mocha.opts
-	// 		// update tdeftly configuration with options supplied in folder
+execAsyncCMD('pwd').then(function(pwd) {
+	tdeftly.projectDirectory = pwd;
+	console.log(pwd);
+	// TODO look for and process opts file before parsing user args below
+	// 		Look for a testingDeftly.opts json file - refer to mocha.opts
+	// 		update tdeftly configuration with options supplied in folder
+	parseArgs(userArgs, tdeftly);
+	if (tdeftly.help || tdeftly.h) help.show();
+
+}).onError(function(err) {
+	console.log(err);
+}).finally(function() {
+	console.log(' - Watching: ' + tdeftly.watch);
+	console.log(' - Verbose : ' + tdeftly.verbose);
+});
