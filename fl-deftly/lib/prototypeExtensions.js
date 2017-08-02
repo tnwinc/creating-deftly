9854e2feea23b297e3867f804f1463a7552d3ac1
StJohn3D <stjohn3d@outlook.com>
2017-08-01 21:40:28 -0400

    seriously adobe, why isn't this implemented?

--- fl-deftly/lib/prototypeExtensions.js
+++ fl-deftly/lib/prototypeExtensions.js
@@ -1,3 +1,17 @@
+//Object Extensions & Polyfill
+if (!Object.keys) {
+    Object.keys = function (obj) {
+        if (obj !== Object(obj)) {
+            throw new TypeError('Object.keys called on a non-object');
+        }
+        var keys = [], p;
+        for (p in obj) {
+            keys.push(p);
+        }
+        return keys;
+    }
+}
+
 //Array Extensions
 if (!Array.prototype.first) Array.prototype.first = function() {return this[0];}
 if (!Array.prototype.last) Array.prototype.last = function() {return this[this.length-1];}
